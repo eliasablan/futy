e4913afacaef663dae8f1be9bec9683260469a50
Elias Ablan <eliasablan93@gmail.com>
2024-06-25 00:17:10 -0400

    update homepage

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default async function Homepage() {
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-      Futy
+      <p>
+        Check the app{" "}
+        <Link className="text-warning underline" href="/dashboard">
+          here
+        </Link>
+      </p>
     </main>
   );
 }
