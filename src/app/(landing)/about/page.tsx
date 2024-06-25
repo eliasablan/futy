f07da6940d3f5a8e13ee7a96518b98625d5fc676
Elias Ablan <eliasablan93@gmail.com>
2024-06-25 00:07:38 -0400

    create about and fake settings page

--- src/app/(landing)/about/page.tsx
+++ src/app/(landing)/about/page.tsx
@@ -1,7 +1,7 @@
-export default async function Homepage() {
+export default async function About() {
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-      Futy
+      About
     </main>
   );
 }
---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "~/components/landing/Header";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <Header />
+      {children}
+    </div>
+  );
+}
